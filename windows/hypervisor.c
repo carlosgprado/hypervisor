b4b89ef855d7a3111dc9e38643947924cb22e28a
Carl OS <carlos.g.prado@gmail.com>
2022-02-20 22:08:42 +0100

    Windows driver skeleton

---
+++ windows/hypervisor.c
@@ -0,0 +1,55 @@
+#include <ntddk.h>
+#include <wdf.h>
+#include <wdm.h>
+
+#include "hypervisor.h"
+
+#pragma alloc_text(INIT, DriverEntry)
+#pragma alloc_text(PAGE, DrvUnload)
+
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath) {
+    NTSTATUS ntStatus = STATUS_SUCCESS;
+    UINT64 uiIndex = 0;
+    PDEVICE_OBJECT pDeviceObject = NULL;
+    UNICODE_STRING usDriverName, usDosDeviceName;
+
+    DbgPrint("[+] DriverEntry called");
+
+    // Create device and alias
+    RtlInitUnicodeString(&usDriverName, L"\Device\Hypervisor");
+    RtlInitUnicodeString(&usDosDeviceName, L"\DosDevices\Hypervisor");
+
+    ntStatus = IoCreateDevice(
+            pDriverObject,
+            0,
+            &usDriverName,
+            FILE_DEVICE_UNKNOWN,
+            FILE_DEVICE_SECURE_OPEN,
+            FALSE,
+            &pDeviceObject
+            );
+
+    if (ntStatus == STATUS_SUCCESS)
+    {
+        pDriverObject->DriverUnload = DrvUnload;
+        pDeviceObject->Flags |= IO_DEVICE_TYPE;
+        pDeviceObject->Flags &= ~DO_DEVICE_INITIALIZING;
+        IoCreateSymbolicLink(&usDosDeviceName, &usDriverName);
+    }
+
+    return ntStatus;
+}
+
+
+VOID DrvUnload(PDRIVER_OBJECT pDriverObject)
+{
+    UNICODE_STRING usDosDeviceName;
+
+    DbgPrint("[+] DrvUnload called");
+
+    RtlInitUnicodeString(&usDosDeviceName, L"\DosDevices\Hypervisor");
+    IoDeleteSymbolicLink(&usDosDeviceName);
+    IoDeleteDevice(pDriverObject->DeviceObject);
+}
+
---
+++ windows/hypervisor.h
@@ -0,0 +1,4 @@
+VOID DrvUnload(PDRIVER_OBJECT pDriverObject);
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath);
+
+
